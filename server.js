a4edcc7a8861b96c6f377e215d74ca100feae3c4
Famuyiwa Emmanuel <famuyiwaemmanuel565@gmail.com>
2025-08-12 23:35:55 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+
+const server = express();
+
+absoluteHtmlPath = __dirname + "/views/index.html";
+absoluteStylesPath = __dirname + "/public";
+
+server.use(express.urlencoded({ extended: false }));
+
+server.use(express.json());
+
+server.use("/public", express.static(absoluteStylesPath));
+
+server.use(cors({ optionsSuccessStatus: 200 }));
+
+let urls = {};
+let counter = 1024;
+
+server.get("/", (req, res) => {
+  res.sendFile(absoluteHtmlPath);
+});
+
+server.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.url;
+
+  try {
+    new URL(originalUrl);
+  } catch {
+    return res.json({ error: "Invalid URL" });
+  }
+
+  const shortId = counter++;
+  urls[shortId] = originalUrl;
+
+  res.json({ original_url: originalUrl, short_url: shortId });
+});
+
+server.get("/api/shorturl/:id", (req, res) => {
+  const id = req.params.id;
+  const originalUrl = urls[id];
+
+  if (!originalUrl) {
+    return res.json({ error: "Invalid request" });
+  }
+
+  res.redirect(originalUrl);
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
